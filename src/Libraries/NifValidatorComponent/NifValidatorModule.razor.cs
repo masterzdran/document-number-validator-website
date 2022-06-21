a0d678b6728a83c76793b00c429c078835ddd018
Nuno Cancelo <nuno.cancelo@gmail.com>
2022-06-21 22:51:14 +0100

    WiP: add nif validator

---
+++ src/Libraries/NifValidatorComponent/NifValidatorModule.razor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Portugal.Nif.Validator;
+
+namespace DocumentNumberValidatorWebsite
+{
+    public partial class NifValidatorModule
+    {
+       
+        private string nif = string.Empty;
+        private readonly NifValidator validator;
+        private bool isValid;
+
+        [Parameter]
+        public string Nif { get => nif; set => nif = value; }
+
+        [Parameter]
+        public bool IsValid { get => isValid; set => isValid = value; }
+
+        public NifValidatorModule()
+        {
+            this.validator = new NifValidator();
+
+        }
+        public async Task<bool> Validate()
+        {
+            await Task.CompletedTask;
+            isValid = validator.Validate(Nif);
+            return  isValid;
+        }
+    }
+}
