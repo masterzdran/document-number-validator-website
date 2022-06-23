0f88ad32e084be736826d8c31aaaed6dd6f43188
Nuno Cancelo <nuno.cancelo@polarising.com>
2022-06-23 12:06:34 +0100

    add IBAN Validation

---
+++ src/Libraries/IBANValidatorComponent/IBANValidatorModule.razor.cs
@@ -0,0 +1,33 @@
+﻿using DocumentNumber.InternationalBankAccountNumber.Validator;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBANValidatorComponent
+{
+    public partial class IBANValidatorModule
+    {
+        private readonly InternationalBankAccountNumberValidator validator;
+
+        [Parameter]
+        public string Iban { get; set; } = string.Empty;
+
+        [Parameter]
+        public bool IsValid { get; set; }
+
+        public IBANValidatorModule()
+        {
+            this.validator = new InternationalBankAccountNumberValidator();
+
+        }
+        public async Task<bool> Validate()
+        {
+            await Task.CompletedTask;
+            IsValid = validator.Validate(Iban);
+            return IsValid;
+        }
+    }
+}
