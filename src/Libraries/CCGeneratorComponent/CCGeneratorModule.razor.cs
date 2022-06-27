564041b8ae9a80d07cd70c768ea0257943aef771
Nuno Cancelo <nuno.cancelo@gmail.com>
2022-06-27 18:45:01 +0100

    CC Generator

---
+++ src/Libraries/CCGeneratorComponent/CCGeneratorModule.razor.cs
@@ -0,0 +1,29 @@
+﻿using DocumentNumber.Portugal.CitizenCard.Generator;
+using Microsoft.AspNetCore.Components;
+
+namespace CCGeneratorComponent
+{
+    public partial class CCGeneratorModule
+    {
+        private readonly ICitizenCardNumberGenerator citizenCardNumberGenerator;
+
+        [Parameter]
+        public string CitizenCard { get; set; }
+        [Parameter]
+        public string Title { get; set; }
+
+        public CCGeneratorModule()
+        {
+            this.citizenCardNumberGenerator = new CitizenCardNumberGenerator();
+            this.CitizenCard = String.Empty;
+            this.Title = String.Empty;
+        }
+        public async Task<string> GenerateRandomDocumentNumber()
+        {
+            await Task.CompletedTask;
+            CitizenCard = citizenCardNumberGenerator.GenerateDocumentNumber();
+
+            return CitizenCard;
+        }
+    }
+}
