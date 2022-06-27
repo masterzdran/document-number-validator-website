3fc11c0c4dc4f60d60898f092d0f7d80f4637644
Nuno Cancelo <nuno.cancelo@gmail.com>
2022-06-27 19:40:08 +0100

    NISS Generator

---
+++ src/Libraries/NissGeneratorComponent/NissGeneratorModule.razor.cs
@@ -0,0 +1,31 @@
+﻿using DocumentNumber.Portugal.Niss.Generator;
+using Microsoft.AspNetCore.Components;
+
+namespace NissGeneratorComponent
+{
+    public partial class NissGeneratorModule
+    {
+        private readonly NissGenerator nissGenerator;
+        public NissGeneratorModule()
+        {
+            this.nissGenerator = new NissGenerator();
+            this.DocumentNumber = String.Empty;
+            this.Title = String.Empty;
+
+        }
+
+        [Parameter]
+        public string DocumentNumber { get; set; }
+
+        [Parameter]
+        public string Title { get; set; }
+
+        public async Task<string> GenerateRandomDocumentNumber()
+        {
+            await Task.CompletedTask;
+            DocumentNumber = nissGenerator.GenerateDocumentNumber();
+
+            return DocumentNumber;
+        }
+    }
+}
